e9ebbd587872e4557f4e2015147d8f4bd5bc329a
migcabvid <miguelcaballerovidal@gmail.com>
2024-10-16 08:31:07 +0200

    Versión inicial del fichero HolaMundo.java sin colores

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola mundo!");
+        System.out.println("Aquí estamos haciendo pruebas en Git.");
+    }
+}
